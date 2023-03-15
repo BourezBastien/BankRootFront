097dd2c994215b2662d0a0ca355e5e9ed4f1b4d7
Bourez Bastien <Bourezbastien@gmail.com>
2023-03-15 14:14:32 +0100

    feat(common): add eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+import {ClientListComponent} from './components/client-list/client-list.component';
+import {HomepageComponent} from './pages/homepage/homepage.component';
+import {LoginComponent} from './pages/login/login.component';
+import {RegistrationComponent} from './pages/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent,
+    HomepageComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +24,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
