794b6a0c705b54eef4c5798b0e9c8588225ccc71
Bourez Bastien <Bourezbastien@gmail.com>
2023-03-22 12:30:57 +0100

    feat(guard): add guard service

---
+++ src/app/_guard/auth-guad.service.ts
@@ -0,0 +1,18 @@
+import {inject} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot} from "@angular/router";
+import {AuthService} from "../_services/auth.service";
+import {NotificationService} from "../_services/notification.service";
+
+export const canActivate: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  const notification = inject(NotificationService)
+
+  if (!auth.getToken()) {
+    router.navigateByUrl('/login');
+    notification.openError("Erreur", "Veuillez vous authentifier", 5000)
+    return false;
+  }
+
+  return true;
+}
