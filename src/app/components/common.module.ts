047298bb1c167e31a84fa0c54ea5e9c6ec87c1cb
BourezBastien <Bourezbastien@gmail.com>
2023-03-23 16:41:20 +0100

    feat(common-module): add client bank account and client cards request components

--- src/app/components/common.module.ts
+++ src/app/components/common.module.ts
@@ -3,11 +3,12 @@ import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
 import {ClientListComponent} from "./client-list/client-list.component";
 import {AdminCardsComponent} from "./admin-cards/admin-cards.component";
+import {ClientBankAccountComponent} from './client-bank-account/client-bank-account.component';
+import {ClientCradsRequestsComponent} from './client-crads-requests/client-crads-requests.component';
 
 @NgModule({
-  declarations: [],
-  imports: [HeaderComponent, FooterComponent, ClientListComponent, AdminCardsComponent],
-  exports: [HeaderComponent, FooterComponent, ClientListComponent, AdminCardsComponent],
+  imports: [HeaderComponent, FooterComponent, ClientListComponent, AdminCardsComponent, ClientBankAccountComponent, ClientCradsRequestsComponent],
+  exports: [HeaderComponent, FooterComponent, ClientListComponent, AdminCardsComponent, ClientBankAccountComponent, ClientCradsRequestsComponent],
 })
 export class CommonComposentModule {
 }
