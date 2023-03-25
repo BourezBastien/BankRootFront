b7e93ce5dd80c733dbc451ecacc04257b06c5121
Bourez Bastien <Bourezbastien@gmail.com>
2023-03-25 18:53:04 +0100

    feat(homepage): add router link for homepage

--- src/app/pages/homepage/homepage.module.ts
+++ src/app/pages/homepage/homepage.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomepageComponent} from './homepage.component';
 import {CommonComposentModule} from "../../components/common.module";
+import {RouterLink} from "@angular/router";
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import {CommonComposentModule} from "../../components/common.module";
   imports: [
     CommonModule,
     CommonComposentModule,
+    RouterLink
   ]
 })
 export class HomepageModule {
