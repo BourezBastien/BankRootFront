4da083db5308e3057af59cdc8d69341b9818f6d4
Bourez Bastien <Bourezbastien@gmail.com>
2023-03-28 17:30:03 +0200

    feat(admin-cards): add list of request on cards click

--- src/app/components/admin-cards/admin-cards.component.ts
+++ src/app/components/admin-cards/admin-cards.component.ts
@@ -2,37 +2,38 @@ import {Component, OnInit} from '@angular/core';
 import {ApiService} from "../../_services/api.service";
 import {NotificationService} from "../../_services/notification.service";
 import {BrowserModule} from "@angular/platform-browser";
+import {CommonModule} from "@angular/common";
 
 @Component({
   standalone: true,
   selector: 'app-admin-cards',
   templateUrl: './admin-cards.component.html',
   styleUrls: ['./admin-cards.component.scss'],
-  imports: [BrowserModule]
+  imports: [BrowserModule, CommonModule]
 })
 export class AdminCardsComponent implements OnInit {
   requests!: any[];
+  modal!: any;
 
   constructor(private apiservice: ApiService, private notificationService: NotificationService) {
   }
 
   DEPOSIT = 0;
-  WHITHDRAW = 0;
+  WITHDRAW = 0;
 
   async ngOnInit() {
     const requests = await this.getRequests();
     this.requests = requests;
     if (requests) {
       requests.map((item: any) => {
-          console.log(item);
           if (item.type === "deposit") {
             this.DEPOSIT++
           } else if (item.type === "withdraw") {
-            this.WHITHDRAW++
+            this.WITHDRAW++
           }
         }
       )
-      return this.notificationService.openInfo("Demandes", `Vous avez ${this.DEPOSIT + this.WHITHDRAW} demande(s) en attente`)
+      return this.notificationService.openInfo("Demandes", `Vous avez ${this.DEPOSIT + this.WITHDRAW} demande(s) en attente`)
     } else {
       return this.notificationService.openInfo("Demandes", "Aucune nouvelles demandes")
     }
@@ -47,5 +48,25 @@ export class AdminCardsComponent implements OnInit {
     });
   }
 
+  openModal(type?: string) {
+
+    // Get the modal
+    this.modal = document.getElementById("myModal");
+
+    this.requests.map((item) => {
+      if (type === "deposit") {
+        if (item.type != "deposit") return this.notificationService.openInfo("Information", "Aucune requête(s) de ce type!");
+        this.modal.style.display = "flex";
+      } else if (type === "withdraw") {
+        if (item.type != "withdraw") return this.notificationService.openInfo("Information", "Aucune requête(s) de ce type!");
+        this.modal.style.display = "flex";
+      } else return this.notificationService.openError("Erreur", "Cette action ne dispose d'aucun type.");
+    })
+
+  }
+
+  closeModal() {
+    this.modal.style.display = "none";
+  }
 
 }
